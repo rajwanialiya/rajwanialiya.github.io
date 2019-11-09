9612581cca6b2469fc1c5069868fe3ae14bc7c69
rajwanialiya <rajwani.aliya@gmail.com>
2019-11-09 11:22:07 -0500

    added project with left image component

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,38 @@
     <nav-bar></nav-bar>
     <socials></socials>
     <intro class="section"></intro>
-    <project-left class="section"></project-left>
+    <project-left class="section">
+      <template slot="description">
+        <div class="image">
+          <a href="pages/flashfoodSite.html"><img src="./components/assets/Website-Mockup.png"></a>
+        </div>
+        <div>
+          <div class="intro">
+            <p>Web Development & UI/UX</p>
+            <h2>Flashfood.com</h2>
+          </div>
+          <div class="main">
+            <p class="blurb">As Flashfood grew to reach a wider audience, its brand evolved and features were added to the app. My task for this project was to work with the Head of Product & Design and the company's CTO to design and implement a scalable updated main website that reflected these changes.</p>
+            <div>
+              <a href="pages/flashfoodSite.html" class="visit"><img src="./components/assets/casestudy.svg"></a>
+              <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="./components/assets/globe.svg"></a>
+            </div>
+            </div>
+            <ul class="skills">
+                <li>Responsive</li>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>AJAX</li>
+            </ul>
+            <div class="nextMobile">
+              <a href="pages/flashfoodSite.html" class="visit"><img src="assets/casestudy.svg"></a>
+              <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="assets/globe.svg"></a>
+            </div>
+            <div class="empty"></div>
+          </div>
+      </template>
+    </project-left>
   </div>
 </template>
 
@@ -39,7 +70,7 @@ export default {
 }
 
 .view-content {
-  max-width: 1200px;
+  max-width: 1000px;
   margin: 0 0; 
   overflow: hidden;
 }
@@ -47,5 +78,9 @@ export default {
 .section {
   width: 100vw; 
   height: 100vh; 
+}
+
+h2, h3, h4, h5, h6, p, a {
+  margin-bottom: 0.5em; 
 }
 </style>
--- src/components/project-left.vue
+++ src/components/project-left.vue
@@ -1,31 +1,9 @@
 <template>
   <div class="view-wrap">
     <div class="view-content">
-        <div class="project"> 
-            <div class="image">
-                <h2 class="textOverlay">Read More</h2>
-                <a href="pages/flashfoodSite.html"><img src="assets/Website-Mockup.png"></a>
-            </div>
-            <div class="description1">
-                <p class="category">Web Development & UI/UX</p>
-                <h2 class="title">Flashfood.com</h2>
-                <div id="description">
-                <p class="blurb">As Flashfood grew to reach a wider audience, its brand evolved and features were added to the app. My task for this project was to work with the Head of Product & Design and the company's CTO to design and implement a scalable updated main website that reflected these changes.</p>
-                <div class="next">
-                    <a href="pages/flashfoodSite.html" class="visit"><img src="assets/casestudy.svg"></a>
-                    <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="assets/globe.svg"></a>
-                </div>
-                </div>
-                <ul class="skills">
-                <slot name="skills"></slot>
-                </ul>
-                <div class="nextMobile">
-                <a href="pages/flashfoodSite.html" class="visit"><img src="assets/casestudy.svg"></a>
-                <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="assets/globe.svg"></a>
-                </div>
-                <!-- <div class="moreDiv"><button onclick="window.location.href='pages/flashfoodSite.html'" class="more">Read More →</button></div> -->
-            </div>
-        </div>
+      <div class="portfolio">
+        <slot name="description"></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -40,32 +18,25 @@ export default {
 </script>
 
 <style>
-
-.portfolios {
-  position: relative; 
-  top: 15vh; 
-  padding-bottom: 10%; 
-  overflow-x: hidden; 
+.portfolio {
+    text-align: right;
+    display: flex; 
 }
 
-.portfolio{
-    margin-left: 10%; 
-    justify-content: flex-end;  
-    width: 80%; 
-    padding: 50px; 
-    padding-bottom: 0; 
-    overflow-x: hidden; 
-    height: 93vh; 
+.intro {
+    display: block
 }
 
-.project{
-    display:flex; 
-    flex-direction: row;  
+.main {
+    display: flex;
+    justify-content: flex-end; 
+    margin: 20px 0;
+    position: relative;
 }
 
-.image{
-    width: 100%; 
-    z-index: 10; 
+.blurb{ 
+    background-color: rgba(243, 247, 253, 0.9);  
+    padding: 20px 30px; 
 }
 
 .image img:hover{ 
@@ -77,143 +48,31 @@ export default {
     height: 100%; 
 }
 
-.textOverlay{
-    position: absolute; 
-    top: 65vh; 
-    left: 17%; 
-    visibility: hidden;
-    font-size: 2rem;  
-    z-index: 20; 
-}
-
-.textOverlay2{
-    position: absolute; 
-    top: 158.4vh; 
-    left: 72%; 
-    visibility: hidden;
-    font-size: 2rem; 
-    z-index: 20;  
-}
-
-.textOverlay3{
-    position: absolute; 
-    top: 252.4vh; 
-    left: 16%; 
-    visibility: hidden;
-    font-size: 2rem;  
-    z-index: 20; 
-}
-
-.image:hover .textOverlay, .image:hover .textOverlay2, .image:hover .textOverlay3{
-    visibility: visible; 
-    color: white;
-    z-index: 100; 
-}
-
-
-.description1, .description2, .description3{
-    width: 60%; 
-}
-
 .image img{
     width: 100%; 
-}
-
-.category, .category2{
-    margin-bottom: 5px; 
-}
-
-.blurb, .blurb2{
-    z-index: 11; 
-}
-
-.category, .title, .blurb{
-    text-align: right; 
-}
-
-.category2, .title2, .blurb2{
-    text-align: left; 
-}
-
-.title, .title2{
-    margin-top: -2%; 
-}
-
-#description{
-    display: flex; 
-    flex-direction: row; 
-    margin-top: 30px; 
-    margin-bottom: 30px;
-}
-
-.blurb{ 
-    background-color: rgb(243, 247, 253);  
-    opacity: 0.9; 
-    color: black; 
-    padding: 20px 30px; 
-    position: relative;
-    left: -14vh; 
-    margin-bottom: 0; 
-}
-
-.blurb2{ 
-    background-color: rgb(243, 247, 253);  
-    opacity: 0.9; 
-    color: black; 
-    padding: 20px 30px;
-    position: relative;
-    left: 16vh; 
+    position: relative; 
+    /* -webkit-filter: grayscale(100%);
+    filter: grayscale(100%);  */
 }
 
 .visit{
     justify-content: flex-end;
     align-items: flex-end;
     display: flex;
-    width: 4.8vh;
+    width: 30px; 
+    margin-left: 20px; 
 }
 
-.visit img, .visit2 img{
-    margin-bottom: 15px;
+.visit img {
+    margin-bottom: 5px;
     width: 100%; 
 }
 
-.visit img:hover, .visit2 img:hover{
+.visit img:hover {
     opacity: 0.5;
 }
 
-.visit2{
-    justify-content: flex-start;
-    align-items: flex-start;
-    display: flex;
-    width: 4.8vh; 
-}
-
-
-.next{
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: flex-end;
-    width: 100%;
-    margin-left: -7vw;
-    height: 10%;
-}
-
 .nextMobile{
-    display: none; 
-}
-
-.next2{
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    width: 100%;
-    margin-right: -7vw;
-    height: 10%;
-}
-
-.nextMobile2{
     display: none; 
 }
 
@@ -224,46 +83,8 @@ export default {
     justify-content: flex-end; 
 }
 
-.skills2{
-    display: flex; 
-    flex-direction: row; 
-    list-style-type: none;
-    justify-content: flex-start; 
-}
-
-.skills li{
-    margin-left: 10px; 
-}
-
-.skills2 li{
-    margin-right: 10px; 
-}
-
-.moreDiv{
-    height: 62%; 
-    width: 100%; 
-    display: flex; 
-    justify-content: flex-end; 
-}
-
-.more, .more2{
-    background-color: transparent; 
-    border-color: transparent; 
-    font-weight: 500;
-    font-size: 20px; 
-    text-align: left; 
-    color: darkblue; 
-}
-
-.moreDiv2{
-    height: 56%; 
-    width: 100%; 
-    display: flex; 
-    justify-content: flex-start; 
-}
-
-.more:hover, .more2:hover{
-    font-size: 22px; 
+.skills li {
+    margin-right: 0.7em; 
 }
 
 </style>
