39d6c7f8bd60b15c64266058ecec8962ad94ea8a
rajwanialiya <rajwani.aliya@gmail.com>
2019-11-12 21:22:24 -0500

    changes to home page

--- src/App.vue
+++ src/App.vue
@@ -3,99 +3,101 @@
     <nav-bar></nav-bar>
     <socials></socials>
     <intro class="section"></intro>
-    <project-left class="section">
-      <template slot="description">
-        <div class="image">
-          <a href="pages/flashfoodSite.html"><img src="./components/assets/Website-Mockup.png"></a>
-        </div>
-        <div>
-          <div class="intro">
-            <p>Web Development & UI/UX</p>
-            <h2>Flashfood.com</h2>
+    <div class="body">
+      <project-left class="section">
+        <template slot="description">
+          <div class="image">
+            <router-link to="/pages/flashfood-site.vue"><img src="./components/assets/Website-Mockup.png"></router-link>
           </div>
-          <div class="main">
-            <p class="blurb">As Flashfood grew to reach a wider audience, its brand evolved and features were added to the app. My task for this project was to work with the Head of Product & Design and the company's CTO to design and implement a scalable updated main website that reflected these changes.</p>
-            <div>
-              <a href="pages/flashfoodSite.html" class="visit"><img src="./components/assets/casestudy.svg"></a>
-              <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="./components/assets/globe.svg"></a>
+          <div>
+            <div class="intro">
+              <p>Web Development & UI/UX</p>
+              <h2>Flashfood.com</h2>
             </div>
+            <div class="main">
+              <p class="blurb">As Flashfood grew to reach a wider audience, its brand evolved and features were added to the app. My task for this project was to work with the Head of Product & Design and the company's CTO to design and implement a scalable updated main website that reflected these changes.</p>
+              <div>
+                <a href="pages/flashfoodSite.html" class="visit"><img src="./components/assets/casestudy.svg"></a>
+                <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="./components/assets/globe.svg"></a>
+              </div>
+              </div>
+              <ul class="skills">
+                  <li>Responsive</li>
+                  <li>JavaScript</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>AJAX</li>
+              </ul>
+              <div class="nextMobile">
+                <a href="pages/flashfoodSite.html" class="visit"><img src="assets/casestudy.svg"></a>
+                <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="assets/globe.svg"></a>
+              </div>
             </div>
-            <ul class="skills">
-                <li>Responsive</li>
-                <li>JavaScript</li>
+        </template>
+      </project-left>
+      <project-right class="section">
+        <template slot="description">
+          <div>
+            <div class="intro">
+              <p>Web Development & Back-End</p>
+              <h2>Food Waste Counter</h2>
+            </div>
+            <div class="main">
+              <div>
+                <a href="pages/flashfoodSite.html" class="visit two"><img src="./components/assets/casestudy.svg"></a>
+                <a href="https://www.flashfood.com/" target="_blank" class="visit two"><img src="./components/assets/globe.svg"></a>
+              </div>
+              <p class="blurb">My task was to create a count-up to display the amount of food waste diverted from landfills by Flashfood users, in real-time. This project involved pulling statistics from the Flashfood database to update the counter regularly, creating an API endpoint to reference when embedding the counter, and animating the count-up to increase interactivity.</p>
+            </div>
+            <ul class="skills second">
+                <li>Javascript</li>
+                <li>SQL</li>
                 <li>HTML</li>
-                <li>CSS</li>
-                <li>AJAX</li>
+                <li>Node.js</li>
+                <li>Git</li>
             </ul>
             <div class="nextMobile">
               <a href="pages/flashfoodSite.html" class="visit"><img src="assets/casestudy.svg"></a>
               <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="assets/globe.svg"></a>
             </div>
           </div>
-      </template>
-    </project-left>
-    <project-right class="section">
-      <template slot="description">
-        <div>
-          <div class="intro">
-            <p>Web Development & Back-End</p>
-            <h2>Food Waste Counter</h2>
+          <div class="image">
+            <a href="pages/counter.html"><img src="./components/assets/Website-Mockup.png"></a>
           </div>
-          <div class="main">
-            <div>
-              <a href="pages/flashfoodSite.html" class="visit two"><img src="./components/assets/casestudy.svg"></a>
-              <a href="https://www.flashfood.com/" target="_blank" class="visit two"><img src="./components/assets/globe.svg"></a>
+        </template>
+      </project-right>
+      <project-left class="section">
+        <template slot="description">
+          <div class="image">
+            <a href="pages/flashfoodSite.html"><img src="./components/assets/Website-Mockup.png"></a>
+          </div>
+          <div>
+            <div class="intro">
+              <p>App Development & UI/UX</p>
+              <h2>Nailed It</h2>
             </div>
-            <p class="blurb">My task was to create a count-up to display the amount of food waste diverted from landfills by Flashfood users, in real-time. This project involved pulling statistics from the Flashfood database to update the counter regularly, creating an API endpoint to reference when embedding the counter, and animating the count-up to increase interactivity.</p>
-          </div>
-          <ul class="skills second">
-              <li>Javascript</li>
-              <li>SQL</li>
-              <li>HTML</li>
-              <li>Node.js</li>
-              <li>Git</li>
-          </ul>
-          <div class="nextMobile">
-            <a href="pages/flashfoodSite.html" class="visit"><img src="assets/casestudy.svg"></a>
-            <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="assets/globe.svg"></a>
-          </div>
-        </div>
-        <div class="image">
-          <a href="pages/counter.html"><img src="./components/assets/Website-Mockup.png"></a>
-        </div>
-      </template>
-    </project-right>
-    <project-left class="section">
-      <template slot="description">
-        <div class="image">
-          <a href="pages/flashfoodSite.html"><img src="./components/assets/Website-Mockup.png"></a>
-        </div>
-        <div>
-          <div class="intro">
-            <p>App Development & UI/UX</p>
-            <h2>Nailed It</h2>
-          </div>
-          <div class="main">
-            <p class="blurb">“Nailed It” is a collaboratively developed app that won second place at the TechXplore Healthcare & AI hackathon. The app uses open-source machine learning libraries and Python to perform early-stage diagnosis of anemia, oxygen deprivation illnesses, and various infections by analyzing the colour discrepancy between users' skin and nails.</p>
-            <div>
-              <a href="pages/flashfoodSite.html" class="visit"><img src="./components/assets/casestudy.svg"></a>
-              <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="./components/assets/globe.svg"></a>
+            <div class="main">
+              <p class="blurb">“Nailed It” is a collaboratively developed app that won second place at the TechXplore Healthcare & AI hackathon. The app uses open-source machine learning libraries and Python to perform early-stage diagnosis of anemia, oxygen deprivation illnesses, and various infections by analyzing the colour discrepancy between users' skin and nails.</p>
+              <div>
+                <a href="pages/flashfoodSite.html" class="visit"><img src="./components/assets/casestudy.svg"></a>
+                <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="./components/assets/globe.svg"></a>
+              </div>
+              </div>
+              <ul class="skills">
+                  <li>Python</li>
+                  <li>TensorFlow</li>
+                  <li>UI</li>
+                  <li>UX</li>
+                  <li>Figma</li>
+              </ul>
+              <div class="nextMobile">
+                <a href="pages/flashfoodSite.html" class="visit"><img src="assets/casestudy.svg"></a>
+                <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="assets/globe.svg"></a>
+              </div>
             </div>
-            </div>
-            <ul class="skills">
-                <li>Python</li>
-                <li>TensorFlow</li>
-                <li>UI</li>
-                <li>UX</li>
-                <li>Figma</li>
-            </ul>
-            <div class="nextMobile">
-              <a href="pages/flashfoodSite.html" class="visit"><img src="assets/casestudy.svg"></a>
-              <a href="https://www.flashfood.com/" target="_blank" class="visit"><img src="assets/globe.svg"></a>
-            </div>
-          </div>
-      </template>
-    </project-left>
+        </template>
+      </project-left>
+    </div>
   </div>
 </template>
 
@@ -109,13 +111,8 @@ import projectRight from './components/project-right'
 export default {
   name: 'app',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+    return {}
   }, 
-  mounted: function () {
-    this.cursor()
-  },
   components: {
     'nav-bar': nav, 
     'intro': intro, 
@@ -124,10 +121,10 @@ export default {
     'project-right': projectRight
   }, 
   methods: {
-    cursor: function () {
       //https://codepen.io/chrisdoble/pen/WQLLVp
     }
-}
+  }
+
 </script>
 
 <style>
---
+++ src/pages/flashfood-site.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>HEY</h1>
+</template>
+
+<script>
+export default {
+  name: 'flashfood-site',
+  data () {
+    return {}
+  } 
+}
+</script>
