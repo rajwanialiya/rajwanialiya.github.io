6c8f00594bcc651b65086fea35a72fe0de162419
Aliya Rajwani <rajwani.aliya@gmail.com>
2020-02-02 12:25:55 -0500

    resume update

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -7,7 +7,7 @@
       <b-navbar-nav class="ml-auto">
         <!-- <b-nav-item>Work</b-nav-item> -->
         <!-- <b-nav-item>View Rand.</b-nav-item> Squarify? Trinaglify?-->
-        <b-nav-item href="https://drive.google.com/file/d/1oyLPo-tcwElkmR5xwdHWPjig_Gju59ka/view?usp=sharing" target="_blank">Resume</b-nav-item>
+        <b-nav-item href="https://drive.google.com/file/d/1MH46qGzZ3ZjoCoZ1Xdn9oVrWwfwFRWqe/view?usp=sharing" target="_blank">Resume</b-nav-item>
         <b-nav-item-dropdown right text="Get In Touch" class="dropdown">
           <b-dropdown-item href="mailto:rajwani.aliya@gmail.com">Email</b-dropdown-item>
           <div class="dropdown-divider"></div>
